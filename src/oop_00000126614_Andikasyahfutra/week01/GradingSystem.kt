41739793874c349cf39b58172f129ad4389bec02
andikas23 <andkaa.s23@gmail.com>
2026-02-04 15:53:23 +0700

    week01: implement grading logic using when expression

--- src/oop_00000126614_Andikasyahfutra/week01/GradingSystem.kt
+++ src/oop_00000126614_Andikasyahfutra/week01/GradingSystem.kt
@@ -4,5 +4,16 @@ fun main() {
     val name= "revlog"
     val score= 80
     println( "nama: $name ,nilai: $score")
+
+    val grade= when (score){
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+    println ("grade kamu = $grade")
+
+
+
 }
 
