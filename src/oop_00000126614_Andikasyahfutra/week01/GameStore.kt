0bc8542123a0a223a3affefa3d80621e8dcaedf4
andikas23 <andkaa.s23@gmail.com>
2026-02-04 18:15:41 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000126614_Andikasyahfutra/week01/GameStore.kt
+++ src/oop_00000126614_Andikasyahfutra/week01/GameStore.kt
@@ -1,2 +1,10 @@
 package oop_00000126614_Andikasyahfutra.week01
 
+fun main() {
+    val gameTitle = "fantastic four"
+    val price = 600000
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+}
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) price * 20 / 100 else price * 10 / 100
