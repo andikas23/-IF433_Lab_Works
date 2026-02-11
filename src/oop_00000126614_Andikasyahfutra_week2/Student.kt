1270f5d493423a230f6d4b995bfc37c98ade23f7
andikas23 <andkaa.s23@gmail.com>
2026-02-11 19:19:10 +0700

    week02: implement selection logic for constructors

--- src/oop_00000126614_Andikasyahfutra_week2/Student.kt
+++ src/oop_00000126614_Andikasyahfutra_week2/Student.kt
@@ -1,30 +1,26 @@
 package oop_00000126614_Andikasyahfutra_week2
 
-class Student (
+
+
+class Student(
     val name: String,
     val nim: String,
     var major: String,
-    var gpa: Double = 0.0)
+    var gpa: Double = 0.0
+) {
 
-
-{
-init {
-    if (nim.length !=5) {
-        println("WARNING:objek tercipta dengan ($nim) yang tidak valid!")
-        println("data mahasiswa $name mungkin akan bermasalah di sistem ")
-    } else {
-        println ("LOG : objek student $name berhasil dialokasikan di memory.")
+    init {
+        if (nim.length != 5) {
+            println("WARNING: objek tercipta dengan ($nim) yang tidak valid!")
+        } else {
+            println("LOG: objek student $name berhasil dialokasikan di memory.")
+        }
     }
 
-}
-    //secondary constructor
-    // wajib memanggil pramary constructor menggunakan this
-    constructor (name: String,nim: String, gpa:Double): this (name, nim, major="Non-Matriculated", gpa){
-        println("LOG : menggunakan constructor jalur umum (tanpa jurusan)")
+    // Secondary constructor (jalur umum)
+    constructor(name: String, nim: String)
+            : this(name, nim, "Non-Matriculated") {
+
+        println("LOG: menggunakan constructor jalur umum (tanpa jurusan)")
     }
 }
-
-
-
-
-
