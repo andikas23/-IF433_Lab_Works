979a5b318440057f4f7efa6c44d8b3478fefb3e3
andikas23 <andkaa.s23@gmail.com>
2026-02-18 15:27:49 +0700

    week03: fix setter using backing field

--- src/oop_00000126614_Andika_week03/Employee.kt
+++ src/oop_00000126614_Andika_week03/Employee.kt
@@ -1,2 +1,16 @@
 package oop_00000126614_Andika_week03
 
+class Employee (val name:String) {
+    var salary: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Error:gaji tidak boleh negatif, set ke 0")
+                field = 0
+            } else {
+                field = value
+            }
+        }
+}
+
+
+
--- src/oop_00000126614_Andika_week03/Main.kt
+++ src/oop_00000126614_Andika_week03/Main.kt
@@ -1,2 +1,7 @@
 package oop_00000126614_Andika_week03
 
+fun main(){
+    val e = Employee("budi")
+    println("---test error---")
+    e.salary=5000
+}
