947e4ae14c45e91dc4f166c8dc78544a554e3fcb
andikas23 <andkaa.s23@gmail.com>
2026-02-24 23:12:37 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000126614_Andika_week04/Main.kt
+++ src/oop_00000126614_Andika_week04/Main.kt
@@ -1,2 +1,22 @@
 package oop_00000126614_Andika_week04
 
+fun main(){
+    println("---testing vehicle---")
+    val generalVehicle= Vehicle( brand = "sepeda ontel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n --- testing car---")
+    val myCar= Car( brand= "toyota", numberOfDoors=4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+
+    println("\n=== Testing Electric Car ===")
+    val electricCar = ElectricCar("Tesla", 4, 85)
+
+    electricCar.accelerate()
+    electricCar.honk()
+    electricCar.openTrunk()
+
+}
