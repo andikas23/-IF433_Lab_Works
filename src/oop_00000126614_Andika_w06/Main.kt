b295bd6b4f80b90d4b66df6ec4d483aceed020a8
andikas23 <andkaa.s23@gmail.com>
2026-03-11 12:38:14 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000126614_Andika_w06/Main.kt
+++ src/oop_00000126614_Andika_w06/Main.kt
@@ -1,4 +1,15 @@
 package oop_00000126614_Andika_w06
 
-class Main {
+fun main() {
+    val lamp = SmartLamp("1", "Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
+
 }
