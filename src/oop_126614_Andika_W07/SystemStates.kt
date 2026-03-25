e41ed9c216cc10729bb584ee55b81ffdb13cfdc3
andikas23 <andkaa.s23@gmail.com>
2026-03-25 15:56:22 +0700

    week07: create ApiResponse sealed class

--- src/oop_126614_Andika_W07/SystemStates.kt
+++ src/oop_126614_Andika_W07/SystemStates.kt
@@ -1,4 +1,11 @@
 package oop_126614_Andika_W07
 
-enum class SystemStates {
+enum class AppState {
+    STARTING,RUNNING,STOPPED
+
+    sealed class ApiResponse{
+        data class Success(val data:String) : ApiResponse()
+        data class Error(val message:String) : ApiResponse()
+        object Loading : ApiResponse()
+    }
 }
