a1418adc6a4e919093324ef29d6c830d9f23954b
andikas23 <andkaa.s23@gmail.com>
2026-03-25 16:20:48 +0700

    week07: (task) create GameItem data class

--- src/oop_126614_Andika_W07/GameManager.kt
+++ src/oop_126614_Andika_W07/GameManager.kt
@@ -1,2 +1,14 @@
 package oop_126614_Andika_W07
 
+object GameManager{
+    var isGameRunning: Boolean = false
+
+    fun startGame(){
+        if (isGameRunning){
+            println("game sudah berjalan!")
+        }else {
+            isGameRunning = true
+            println("Memulai Game Engine")
+        }
+    }
+}
--- src/oop_126614_Andika_W07/GameModels.kt
+++ src/oop_126614_Andika_W07/GameModels.kt
@@ -1,2 +1,16 @@
 package oop_126614_Andika_W07
 
+enum class ItemRarity (val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1);
+
+    data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+    )
+
+}
