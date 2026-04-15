35ede0a19f14ee38e231220223fe3a24347bca24
andikas23 <andkaa.s23@gmail.com>
2026-04-15 15:33:58 +0700

    week08: implement requireNotNull as safer alternative

--- src/oop_126614_Andika_W08/Main.kt
+++ src/oop_126614_Andika_W08/Main.kt
@@ -43,4 +43,15 @@ fun main(){
     } catch (e: NullPointerException) {
         println("CRASH (NPE)! Jangan gunakan !! sembarangan.")
     }
+
+    val apiResponse: Map<String, String?> =
+        mapOf("status" to "200", "token" to null)
+
+    try {
+        val token = requireNotNull(apiResponse["token"]) {
+            "CRITICAL EXCEPTION: Token tidak ditemukan dari server!"
+        }
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
