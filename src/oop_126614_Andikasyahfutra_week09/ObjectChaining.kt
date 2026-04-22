3a8f2c5dcbbf674786f0127caf21dae2371782f6
andikas23 <andkaa.s23@gmail.com>
2026-04-22 15:47:03 +0700

    week09: chain filter, sortedBy,and map on object collection

--- src/oop_126614_Andikasyahfutra_week09/ObjectChaining.kt
+++ src/oop_126614_Andikasyahfutra_week09/ObjectChaining.kt
@@ -1,4 +1,23 @@
 package oop_126614_Andikasyahfutra_week09
 
-class ObjectChaining {
+data class Student(val name: String, val gpa: Double)
+
+fun main() {
+    val students = listOf(
+        Student("Andi", 3.2),
+        Student("Budi", 3.9),
+        Student("Citra", 2.8),
+        Student("Dewi", 3.7)
+    )
+
+    println("=== HONOR STUDENTS PIPELINE ===")
+
+    val honorNames = students
+        .filter { it.gpa >= 3.5 }   //buang yang gpa <3.5
+        .sortedBy { it.name }       // urutkan berdasarkan nama
+        .map { it.name.uppercase() }
+
+    honorNames.forEach {
+        println("Honor Roll: $it")
+    }
 }
