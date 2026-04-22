afeb4b06c1fc3391e4d889700f1b2d083b001ef3
andikas23 <andkaa.s23@gmail.com>
2026-04-22 15:52:07 +0700

    week09: (task) filter CLOSED trades only

--- src/oop_126614_Andikasyahfutra_week09/TradingDashboard.kt
+++ src/oop_126614_Andikasyahfutra_week09/TradingDashboard.kt
@@ -1,2 +1,14 @@
 package oop_126614_Andikasyahfutra_week09
 
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 15, 8.0, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 20, 12.3, "CLOSED"),
+        TradeLog("XRPUSDT", "SHORT", 5, -10.0, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 25, 20.0, "CLOSED")
+    )
+    val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
+}
