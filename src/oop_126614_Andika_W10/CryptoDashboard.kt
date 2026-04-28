e98a1b1452af0d17b2bf51a117947c4929939d17
andikas23 <andkaa.s23@gmail.com>
2026-04-29 00:07:37 +0700

    week10: (task) populate coin repository

--- src/oop_126614_Andika_W10/CryptoDashboard.kt
+++ src/oop_126614_Andika_W10/CryptoDashboard.kt
@@ -1,2 +1,9 @@
 package oop_126614_Andika_W10
 
+fun main() {
+    val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
+
+}
