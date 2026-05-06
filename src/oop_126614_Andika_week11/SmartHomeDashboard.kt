285064748add3ea1ff04f4911ec7dc029048cd9b
andikas23 <andkaa.s23@gmail.com>
2026-05-06 11:38:20 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_126614_Andika_week11/SmartHomeDashboard.kt
+++ src/oop_126614_Andika_week11/SmartHomeDashboard.kt
@@ -8,4 +8,15 @@ fun main() {
     }.also {
         homeDevices.add(it)
     }
+
+//    konfigurasi keamanan also
+    SmartDevice("Ezviz Outdoor", "Camera")
+        .apply {
+            isOnline = true
+            powerLoad = 5
+        }
+        .also {
+            println("(LOG) Kamera terhubung")
+            homeDevices.add(it)
+        }
 }
