3ef0d9b717fc0428fb7387343fb48fa379aee8ce
andikas23 <andkaa.s23@gmail.com>
2026-05-13 09:13:14 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_126614_Andika_W12/SmartFeeder.kt
+++ src/oop_126614_Andika_W12/SmartFeeder.kt
@@ -1,2 +1,17 @@
 package oop_126614_Andika_W12
 
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
+    return availableGram
+}
