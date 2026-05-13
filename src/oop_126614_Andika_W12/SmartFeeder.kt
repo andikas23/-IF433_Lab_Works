74ea50c3d6130831fcbf8a672452c1a25f5d0b72
andikas23 <andkaa.s23@gmail.com>
2026-05-13 09:25:34 +0700

    week12: (task) implement finally block for feeder logging

--- src/oop_126614_Andika_W12/SmartFeeder.kt
+++ src/oop_126614_Andika_W12/SmartFeeder.kt
@@ -38,4 +38,8 @@ fun main() {
     } catch (e: Exception) {
         println("Terjadi error umum: ${e.message}")
     }
+
+    finally {
+        println("Siklus pengecekan dispenser pagi selesai.")
+    }
 }
