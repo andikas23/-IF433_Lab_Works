b80960de705c1ffbb530c1b498959025bb43672b
andikas23 <andkaa.s23@gmail.com>
2026-05-13 08:41:46 +0700

    week12: implement try as expression

--- src/oop_126614_Andika_W12/BasicException.kt
+++ src/oop_126614_Andika_W12/BasicException.kt
@@ -1,5 +1,7 @@
 package oop_126614_Andika_W12
 
+import java.text.NumberFormat
+
 fun divide (a:Int , b: Int): Int{
     try{
         return a / b
@@ -9,4 +11,16 @@ fun divide (a:Int , b: Int): Int{
     }finally {
         println("Division attempt finished")
     }
+}
+
+fun main (){
+    println("=== TEST TRY AS EXPRESSION ===")
+    val inputString = "123A"
+    //try langsung mengembalikan hasil parsing
+    val result : Int = try{
+        Integer.parseInt(inputString)
+    } catch (e: NumberFormatException){
+        -1
+    }
+    println("Hasil parsing :$result")
 }
