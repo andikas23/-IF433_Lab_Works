9adf4a74db5907455e0fffc5b8df3e4d6f4904cb
andikas23 <andkaa.s23@gmail.com>
2026-05-13 08:48:53 +0700

    week12: implement throw keyword for validation

--- src/oop_126614_Andika_W12/CustomExceptions.kt
+++ src/oop_126614_Andika_W12/CustomExceptions.kt
@@ -1,2 +1,19 @@
 package oop_126614_Andika_W12
 
+class InsufficientFundsException(
+    val amount : Double,
+    val balance : Double
+): Exception("Attempted $amount, balance: $balance")
+
+class BankAccount(var balance : Double){
+    fun withdraw(amount : Double){
+        if (amount < 0){
+            throw IllegalArgumentException("amount must be positive")
+        }
+        if (amount > balance){
+            throw InsufficientFundsException(amount, balance)
+        }
+        balance -= amount
+        println("Withdrawal succesful. remaining balance: $balance")
+    }
+}
