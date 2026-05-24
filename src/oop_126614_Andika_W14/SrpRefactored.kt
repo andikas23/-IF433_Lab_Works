117f6a0815ec461988f5d98b6bdfe6888a448bdd
andikas23 <andkaa.s23@gmail.com>
2026-05-24 22:51:46 +0700

    week14: extract UserValidator and UserRepository

---
+++ src/oop_126614_Andika_W14/SrpRefactored.kt
@@ -0,0 +1,12 @@
+package oop_126614_Andika_W14
+
+class UserValidator {
+    fun validate(user: User): Boolean = user.email.contains("@") && user.age >= 18
+}
+
+// cuma akses database
+class UserRepository {
+    fun save(user: User) {
+        println("Saving user: ${user.name} to Database")
+    }
+}
