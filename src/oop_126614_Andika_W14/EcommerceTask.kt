280d4abef9f5b2554d62be2f1f05fb8ea1c4212c
andikas23 <andkaa.s23@gmail.com>
2026-05-24 23:50:57 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_126614_Andika_W14/EcommerceTask.kt
+++ src/oop_126614_Andika_W14/EcommerceTask.kt
@@ -23,3 +23,27 @@ class EmailNotifier : NotificationService {
         println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
     }
 }
+
+interface PricingStrategy {
+    fun calculate(price: Double): Double
+}
+
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double) = price
+}
+
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double) = price * 0.90
+}
+class SafeOrderProcessor(
+    val repo: OrderRepository,
+    val notifier: NotificationService
+) {
+    fun processOrder(itemName: String, basePrice: Double, pricing: PricingStrategy) {
+        val finalPrice = pricing.calculate(basePrice)
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        repo.saveOrder(itemName, finalPrice, pricing::class.simpleName ?: "unknown")
+        notifier.sendNotification(itemName)
+    }
+}
+
